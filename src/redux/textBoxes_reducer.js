4d77a24514dd093d53a1735df9c0187e31652b9c
mecawish <melizacaguimbal34@gmail.com>
2017-08-04 14:56:29 +0800

    try slice for deleting textboxes

--- src/redux/textBoxes_reducer.js
+++ src/redux/textBoxes_reducer.js
@@ -6,7 +6,8 @@ const textBoxesReducer = (state = [], action) => {
 		case 'ADD_TEXTBOX':
 			return state.concat(action.id);
 		case 'DELETE_TEXTBOX':
-			return state.filter((el) => {return el !== action.id});
+			//return state.filter((el) => {return el !== action.id});
+			//return [...state.slice(0, action.id), ...state(action.id + 1)]
 		default:
 			return state;
 	}
